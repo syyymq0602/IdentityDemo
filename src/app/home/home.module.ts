1c26d12cedba03ea60130656d9a9df32ebb40d9f
taujiong <taujiong@outlook.com>
2020-07-01 21:34:06 +0800

    替换home组件页面

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NzButtonModule, NzCardModule, NzTypographyModule } from 'ng-zorro-antd';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [SharedModule, HomeRoutingModule]
+  imports: [
+    SharedModule,
+    HomeRoutingModule,
+    NzCardModule,
+    NzTypographyModule,
+    NzButtonModule,
+    NzSpaceModule
+  ]
 })
 export class HomeModule {
 }
