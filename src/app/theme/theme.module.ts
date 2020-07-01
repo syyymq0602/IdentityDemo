13e1349b3bbadfb3f8c1551eb30fadb4edd5e35d
taujiong <taujiong@outlook.com>
2020-07-01 13:14:01 +0800

    使用 nz-menu 重写 route 组件的界面
    使用 nz-menu 重写 route 组件的界面

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoComponent, RoutesComponent } from './components';
 import { SharedModule } from '../shared/shared.module';
+import { NzMenuModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [LogoComponent,RoutesComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    NzMenuModule
   ],
   exports: [LogoComponent,RoutesComponent]
 })
