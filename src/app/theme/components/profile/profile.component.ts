38d235bc6bd9dbb560ec810c33312e3bddb7726e
taujiong <taujiong@outlook.com>
2020-07-02 09:20:12 +0800

    删除profile组件中无用的代码文件

--- src/app/theme/components/profile/profile.component.ts
+++ src/app/theme/components/profile/profile.component.ts
@@ -1,69 +1,25 @@
 import {
   ApplicationConfiguration,
   AuthService,
-  ConfigState,
-  SessionState,
-  SetLanguage,
+  ConfigState
 } from '@abp/ng.core';
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Navigate, RouterState } from '@ngxs/router-plugin';
 import { Select, Store } from '@ngxs/store';
-import { Observable, fromEvent } from 'rxjs';
-import { map, debounceTime } from 'rxjs/operators';
-import snq from 'snq';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.component.html',
 })
-export class ProfileComponent implements AfterViewInit {
+export class ProfileComponent {
   @Select(ConfigState.getOne('currentUser'))
   currentUser$: Observable<ApplicationConfiguration.CurrentUser>;
 
   @Select(ConfigState.getDeep('localization.languages'))
   languages$: Observable<ApplicationConfiguration.Language[]>;
 
-  smallScreen = window.innerWidth < 992;
-
-  get defaultLanguage$(): Observable<string> {
-    return this.languages$.pipe(
-      map(
-        languages =>
-          snq(
-            () => languages.find(lang => lang.cultureName === this.selectedLangCulture).displayName,
-          ),
-        '',
-      ),
-    );
-  }
-
-  get dropdownLanguages$(): Observable<ApplicationConfiguration.Language[]> {
-    return this.languages$.pipe(
-      map(
-        languages =>
-          snq(() => languages.filter(lang => lang.cultureName !== this.selectedLangCulture)),
-        [],
-      ),
-    );
-  }
-
-  get selectedLangCulture(): string {
-    return this.store.selectSnapshot(SessionState.getLanguage);
-  }
-
   constructor(private store: Store, private authService: AuthService) {}
-
-  ngAfterViewInit() {
-    fromEvent(window, 'resize')
-      .pipe(debounceTime(150))
-      .subscribe(() => {
-        this.smallScreen = window.innerWidth < 992;
-      });
-  }
-
-  onChangeLang(cultureName: string) {
-    this.store.dispatch(new SetLanguage(cultureName));
-  }
 
   logout() {
     this.authService.logout().subscribe(() => {
