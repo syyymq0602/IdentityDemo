990334d9956dd1c472f42dcf4f565debfd11db98
XieTeng <2834358457@qq.com>
2020-10-19 19:20:44 +0800

    顶部主菜单导航栏添加文件、编辑、帮助、其它几个导航按钮

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { ABP } from '@abp/ng.core';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+// 导航栏路由管理
 const routes: Routes = [
   {
     path: '',
@@ -24,6 +25,98 @@ const routes: Routes = [
   {
     path: 'setting-management',
     loadChildren: () => import('@abp/ng.setting-management').then(m => m.SettingManagementModule)
+  },
+  {
+    //自定义导航栏路由
+    path: 'test',
+    loadChildren: () => import('@abp/ng.identity').then(m => m.IdentityModule),
+    data: {
+      routes: {
+        name: '文件',
+        order: 101,
+        iconClass: 'fas fa-question-circle',
+        children: [
+          {
+            path: 'child',
+            name: '添加',
+            order: 1,
+          },
+          {
+            path: 'child',
+            name: '修改',
+            order: 2,
+          },
+          {
+            path: 'child',
+            name: '搜索',
+            order: 3,
+          },
+
+        ],
+      } as ABP.Route,
+    }
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('@abp/ng.identity').then(m => m.IdentityModule),
+    data: {
+      routes: {
+        name: '编辑',
+        order: 101,
+        iconClass: 'fas fa-question-circle',
+        children: [
+          {
+            path: 'child',
+            name: '添加',
+            order: 1,
+          },
+          {
+            path: 'child',
+            name: '删除',
+            order: 2,
+          },
+
+        ],
+      } as ABP.Route,
+    }
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('@abp/ng.identity').then(m => m.IdentityModule),
+    data: {
+      routes: {
+        name: '帮助',
+        order: 101,
+        iconClass: 'fas fa-question-circle',
+        children: [
+          {
+            path: 'child',
+            name: '帮助文档',
+            order: 1,
+          },
+
+        ],
+      } as ABP.Route,
+    }
+  },
+  {
+    path: 'test',
+    loadChildren: () => import('@abp/ng.identity').then(m => m.IdentityModule),
+    data: {
+      routes: {
+        name: '其它',
+        order: 101,
+        iconClass: 'fas fa-question-circle',
+        children: [
+          {
+            path: 'child',
+            name: '操作指南',
+            order: 1,
+          },
+
+        ],
+      } as ABP.Route,
+    }
   }
 ];
 
